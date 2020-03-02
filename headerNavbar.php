58774c32f6ec611753199cf2998c1e70a768cc0d
BachHunter <bachlt6996@gmail.com>
2020-03-02 09:37:04 +0700

    create Shopee

---
+++ headerNavbar.php
@@ -0,0 +1,82 @@
+<nav class="header__navbar">
+    <ul class="header__navbar-list">
+        <li class="header__navbar-item header__navbar-item--has-qr header__navbar-item--separate">
+            Vào cửa hàng trên ứng dụng Shopee
+            <!-- Header QR code -->
+            <div class="header__qr">
+                <img src="./assets/img/qr_code.png" alt="QR code" class="header_qr-img">
+                <div class="header__qr-app">
+                    <a href="" class="header__qr-link">
+                        <img src="./assets/img/google.png" alt="Google Play" class="header__qr-dowload-img">
+                    </a>
+                    <a href="" class="header__qr-link">
+                        <img src="./assets/img/appstore.png" alt="App Store" class="header__qr-dowload-img">
+                    </a>
+                </div>
+            </div>
+        </li>
+        <li class="header__navbar-item">
+            <span class="header__navbar-tittle--no-pointer"> Kết nối</span>
+            <a href="" class="header__navbar-icon-link">
+                <i class="header__navbar-icon fab fa-facebook"></i>
+            </a>
+            <a href="" class="header__navbar-icon-link">
+                <i class="header__navbar-icon fab fa-instagram"></i>
+            </a>
+        </li>
+    </ul>
+
+    <ul class="header__navbar-list">
+        <li class="header__navbar-item header__navbar-item--has-notify">
+            <a href="#" class="header__navbar-item-link">
+                <i class="far fa-bell"></i>
+                Thông báo
+            </a>
+            <div class="header__notify">
+                <header class="header__notify-header">
+                    <h3>Thông báo mới nhận</h3>
+                </header>
+                <ul class="header__notify-list">
+                    <li class="header__notify-item header__notify-item--view">
+                        <a href="" class="header__notify-link">
+                            <img src="https://cf.shopee.vn/file/e226d2220322717d341f711b4c820852_tn" alt="" class="header__notify-img">
+                            <div class="header__notify-info">
+                                <span class="header__notify-name">Set DATE 2022 OHUI Dưỡng Trắng Và Trị Thâm </span>
+                                <span class="header__notify-description">Set dưỡng trắng da Ohui Extreme White 5 món</span> 
+                            </div>
+                        </a>
+                    </li>
+                    <li class="header__notify-item">
+                        <a href="" class="header__notify-link">
+                            <img src="https://cf.shopee.vn/file/ccfe95f7c3338c0fb8571242d247a020" alt="" class="header__notify-img">
+                            <div class="header__notify-info">
+                                <span class="header__notify-name">QUẦN ĐI BIỂN - DU LỊCH</span>
+                                <span class="header__notify-description">Chất liệu vải thấm hút mồ hôi tốt và co dãn thoải mái.</span> 
+                            </div>
+                        </a>
+                    </li>
+                    <li class="header__notify-item">
+                        <a href="" class="header__notify-link">
+                            <img src="https://cf.shopee.vn/file/fb96fed2f822fef801a1853872c08b84_tn" alt="" class="header__notify-img">
+                            <div class="header__notify-info">
+                                <span class="header__notify-name">TAY CẦM GAME ĐƠN RUNG USB</span>
+                                <span class="header__notify-description">Dùng trên 2 hệ máy : PC & Xbox 360</span> 
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+                <footer class="header__notify-footer">
+                    <a href="" class="header__notify-footer-btn">Xem tất cả</a>
+                </footer>
+            </div>    
+        </li>
+        <li class="header__navbar-item">
+            <a href="#" class="header__navbar-item-link">
+                <i class="far fa-question-circle"></i>
+                Trợ giúp
+            </a>
+        </li>
+        <li class="header__navbar-item header__navbar-item--strong header__navbar-item--separate ">Đăng ký</li>
+        <li class="header__navbar-item header__navbar-item--strong">Đăng nhập</li>
+    </ul>
+</nav>
